097713a7c49a754ba89dca9729fbef2f71255b53
Kevin Malomba <malombakevin@gmail.com>
2023-01-23 15:48:35 +0300

    style:Mobile responsiveness-Optimize responsiveness

--- sidebar.js
+++ sidebar.js
@@ -7,8 +7,31 @@ function tarehe() {
 	let monthToday = dateToday.getMonth()
 	let yearToday = dateToday.getFullYear()
 
+	let dayToday = dateToday.getDay()
+
+	if (dayToday ==0){
+		document.querySelector('.day').innerHTML = "Sunday";
+	}
+	else if(dayToday==1){
+		document.querySelector('.day').innerHTML = "Monday"
+	}
+	else if(dayToday==2){
+		document.querySelector('.day').innerHTML = "Tuesday"
+	}
+	else if(dayToday==3){
+		document.querySelector('.day').innerHTML = "Wednesday"
+	}
+	else if(dayToday==4){
+		document.querySelector('.day').innerHTML = "Thursday"
+	}else if(dayToday==5){
+		document.querySelector('.day').innerHTML = "Friday"
+	}
+	else {
+		document.querySelector('.day').innerHTML = "Saturday"
+	}
+
 	if(monthToday==0){
-		document.querySelector('.year').innerHTML = `Today:  ${todayDate} January ${yearToday}`
+		document.querySelector('.year').innerHTML = ` ${todayDate}-January-${yearToday}`
 	}
 
 	else if(monthToday ==1){
